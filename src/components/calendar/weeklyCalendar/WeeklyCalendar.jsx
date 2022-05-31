c234d26facfbc8cccd3b53ec29b6d9dfdf1b3600
Inyoung Park <rkduddl1rl@gmail.com>
2022-05-30 20:00:33 -0600

    Weekly Cal - grouped by position finished & CSS

--- src/components/calendar/weeklyCalendar/WeeklyCalendar.jsx
+++ src/components/calendar/weeklyCalendar/WeeklyCalendar.jsx
@@ -12,11 +12,12 @@ const WeeklyCalendar = () => {
     const startDayOfWeek = selectedDay.clone().startOf("day");
     const endDayOfWeek = startDayOfWeek.clone().add(6, "days");
     //days in week should include startDayOfWeek
-    const weekCalArray = [startDayOfWeek.format("DD")];
+    const weekCalArray = [startDayOfWeek.format()];
     //If you dont insert clone(), startDayOfweek will be incresed everytime in this loop.
     while (startDayOfWeek.add(1, "days").diff(endDayOfWeek) <= 0) {
-      weekCalArray.push(startDayOfWeek.format("DD"));
+      weekCalArray.push(startDayOfWeek.format());
     }
+    console.log(weekCalArray);
     setDaysInWeek(weekCalArray);
   }, [selectedDay]);
 
@@ -26,13 +27,24 @@ const WeeklyCalendar = () => {
         selectedDay={selectedDay}
         setSelectedDay={setSelectedDay}
       />
-      <div className="WeeklyCal-dates">
-        {daysInWeek?.map((day, index) => (
-          <div className="WeeklyCal-date" key={`daysInWeek ${index}`}>
-            {day}
-          </div>
-        ))}
-      </div>
+      <>
+        {daysInWeek?.map((day, index) => {
+          const isWeekend =
+          moment(day).day() === 0||moment(day).day()===6 ? "Weekend" : "";
+          const isToday =
+            day === moment().startOf("day").format() ? "Today" : "";
+            console.log("isWeekend", moment(day).day());
+
+          return (
+            <div
+              className={`WeeklyCal-date ${isToday} ${isWeekend}`}
+              key={`daysInWeek ${index}`}
+            >
+              {moment(day).format("ddd DD")}
+            </div>
+          );
+        })}
+      </>
       <WeeklyCalendarBody selectedDay={selectedDay} />
     </div>
   );
--- src/components/calendar/weeklyCalendar/scheduleBody/DisplayMySched.jsx
+++ src/components/calendar/weeklyCalendar/scheduleBody/DisplayMySched.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FaUserCircle } from "react-icons/fa";
 
 const DisplayMySched = (props) => {
   const {myProfile, mySched, displaySched} = props;
 
   return (
     <>
-    <div className="myProfile">
+    <div className="WeeklyCal-Profiles" id="myProfile">
+        {myProfile?.picture?<div>{myProfile.picture}</div>:<FaUserCircle fontSize="3rem"/>}
         <div>{myProfile?.name}</div>
         <div>{myProfile?.position}</div>
         <div>Me</div>
