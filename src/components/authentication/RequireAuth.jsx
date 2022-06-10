d93f126e13573d02768e5bf51c10f023693bdcc3
Jeremiah Silva <100396827+Jeremiasilva@users.noreply.github.com>
2022-06-10 12:22:14 -0600

    Updating Comments

--- src/components/authentication/RequireAuth.jsx
+++ src/components/authentication/RequireAuth.jsx
@@ -1,18 +1,21 @@
+/** Import resorces from files, app and ruoter. It work as a "bridge" to produce the funcionality of the app. */
 import React from "react";
 import { useContext } from "react";
-
 import { Navigate } from "react-router-dom";
 import { LoginContext } from "./LoginProvider";
 
+/**  UI allowing the user to keep in the page. */
 function RequireAuth({ children }) {
   let auth = useContext(LoginContext);
 
+// Condition. 
   if (!auth.user) {
-    //If user is not logged in, redirect to login page
+  //If user is not logged in, redirect to login page
     return <Navigate to="/login" />;
   }
-
+  // Returns the result of value from children.
   return children;
 }
 
+/** It's part of the ES6 module system thats defines a default export. In the case of Wink project RequireAuth. */
 export default RequireAuth;
