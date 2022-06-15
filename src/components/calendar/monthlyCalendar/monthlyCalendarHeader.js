2a4d4ae8b4ef3d9fa3eba4ea093a12c8686be2fb
Jeremiah Silva <100396827+Jeremiasilva@users.noreply.github.com>
2022-06-14 21:32:21 -0600

    Updating Comments

--- src/components/calendar/monthlyCalendar/monthlyCalendarHeader.js
+++ src/components/calendar/monthlyCalendar/monthlyCalendarHeader.js
@@ -1,10 +1,12 @@
+/** Import resorces from files. It work as a "tools" to produce the functionality of the app. */
 import React from 'react'
 
+/** The authProvider methods must return a Promise. */
 const MonthlyCalendarHeader = (props) => {
     const {day,setday} = props
     const monthInHeader = day.format('MMM YYYY')
     
-    
+/** Returns the result of className denominated Monthly-header. */
   return (
     <div className='Monthly-header'>
 
@@ -12,5 +14,5 @@ const MonthlyCalendarHeader = (props) => {
   )
 }
 
+/** It's part of the ES6 module system thats defines a default export. In the case of Wink project MonthlyCalendarHeader. */
 export default MonthlyCalendarHeader
-
