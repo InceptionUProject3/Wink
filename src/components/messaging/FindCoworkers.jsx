fe954b5b27051af5a8cb8059aa64806bf2dd6c06
lmifflen <lmifflen@gmail.com>
2022-06-23 18:08:12 -0600

    mapped list of coworkers

--- src/components/messaging/FindCoworkers.jsx
+++ src/components/messaging/FindCoworkers.jsx
@@ -1,46 +1,56 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from "react";
 import { StoreContext } from "../authentication/StoreProvider";
 import { LoginContext } from "../authentication/LoginProvider";
 
-
-
 const FindCoworkers = (props) => {
+  const authContext = useContext(LoginContext);
+  const storeContext = useContext(StoreContext);
+  const user = storeContext.store;
+  const [coworkers, setCoworkers] = useState([]);
 
-    const authContext = useContext(LoginContext);
-    const storeContext = useContext(StoreContext);
-    const user = storeContext.store;
-    const [ coworkers, setCoworkers ] = useState([]);
+  console.log("user", user);
+  useEffect(() => {
+    const getCoworkers = async (theUser) => {
+      let userToSend = JSON.stringify(theUser);
+      console.log("sending userId: " + typeof userToSend);
+      const response = await fetch("/api/coworkers", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: userToSend,
+      });
+      const data = await response.text();
+      console.log("getting store data", data);
+      const userData = JSON.parse(data);
+      console.log(response);
+      setCoworkers(userData);
+      console.log("we have the user data", userData);
+    };
+    if (user) {
+      getCoworkers(user);
+    }
+  }, [user]);
 
-console.log("user", user);
-    useEffect(() => {
-        const getCoworkers = async (theUser) => {
-          let userToSend = JSON.stringify(theUser);
-          console.log("sending userId: " + typeof(userToSend));
-          const response = await fetch("/api/coworkers", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: userToSend,
-          });
-          const data = await response.text();
-          console.log("getting store data", data);
-          const userData = JSON.parse(data);
-          console.log(response);
-          setCoworkers(userData);
-          console.log("we have the user data", userData);
-        };
-        if (user) {
-          getCoworkers(user);
-        }
-      }, [user]);
-
-  return (<div>
-    <h1>Find coworkers</h1>
-    <div> {JSON.stringify(coworkers)}</div>
-    </div>
-    )
+  return (
+    <div>
+      <h1>Find coworkers</h1>
      
-       }
+      <div>
+        {coworkers ? (
+          coworkers.map((profile, index) => {
+            return (
+              <div key={index} className="user">
+                <div>{profile.user.firstname} {profile.user.lastname}, {profile.userprofile.name} </div>
+              </div>
+            );
+          })
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default FindCoworkers
+export default FindCoworkers;
