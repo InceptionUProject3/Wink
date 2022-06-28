ccc710b83a87cc5f72c1b071b8c9778b389a962b
Siri <sirilakku01@gmail.com>
2022-06-28 15:59:37 -0600

    commented console.logs

--- src/components/calendar/monthlyCalendar/DisplayMonthlySched.jsx
+++ src/components/calendar/monthlyCalendar/DisplayMonthlySched.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DisplayMonthlySched = (props) => {
      const {monthsArray,scheduleHrs,timezone,storeOpen} = props
-     console.log("scheduleHrs" , scheduleHrs)
+     //console.log("scheduleHrs" , scheduleHrs)
   return (
     <div></div>
   )
