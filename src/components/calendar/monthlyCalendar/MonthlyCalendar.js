1d7fba5e27610bd5efca1337aa08077fc2245521
Siri <sirilakku01@gmail.com>
2022-07-10 21:58:29 -0600

    Moved monthly schedule component into new file

--- src/components/calendar/monthlyCalendar/MonthlyCalendar.js
+++ src/components/calendar/monthlyCalendar/MonthlyCalendar.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import moment from "moment";
 import { Container } from "@mui/system";
 import MonthlyCalendarHeader from "./MonthlyCalendarHeader";
 import AddEvent from "./AddEvent";
 import "./monthlyCalendar.css";
 import { LoginContext } from "../../authentication/LoginProvider";
 import { StoreContext } from "../../authentication/StoreProvider";
-import DisplayMonthlySched from "./DisplayMonthlySched";
+import DisplayHolidays from "./DisplayHolidays";
+import MonthlySchedBar from "./MonthlySchedBar";
 
 const MonthlyCalendar = (props) => {
   const userId = useContext(LoginContext).user?.id || 9;
@@ -31,62 +31,13 @@ const MonthlyCalendar = (props) => {
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const firstDayOfMonth = new Date(year, month, 1);
   const [monthsArray, setMonthsArray] = useState();
-  const [addEvent, setAddEvent] = useState();
+  const [addEvent, setAddEvent] = useState(false);
   const [myMonSched, setMonSched] = useState();
   const [holidaysOfMonth, setholidaysOfMonth] = useState();
   const startOfMonth = today?.clone().startOf("months");
   const endOfMonth = today?.clone().endOf("months");
 
   // console.log("startOfMonth",startOfMonth)
-
-  useEffect(() => {
-    const getAllSchedules = async () => {
-      try {
-        const monthStart = startOfMonth.clone().format("YYYY-MM-DD");
-        //console.log("today, startOfMonth", today, startOfMonth);
-        const res = await fetch(
-          `/api/schedule/monthly?storeId=${storeId}&userId=${userId}&startOfMonth=${monthStart}`
-        );
-        const scheduleData = await res.json();
-        //console.log('fetched data', scheduleData)
-        setMonSched(() => scheduleData.mySchedules[0].schedules);
-        } catch (err) {
-        console.log("failed to fetch schedule data", err);
-        setMonSched(() => null);
-      }
-    };
-    startOfMonth && getAllSchedules();
-  }, [today, storeId]);
-
-  //console.log("my month Schedule", myMonSched);
-
-  useEffect(() => {
-    const getMonHolidays = async () => {
-      const startOfMonth = today?.clone().startOf("months");
-      const endOfMonth = today?.clone().endOf("months");
-      const startOfHoliday = startOfMonth.clone().format("YYYY-MM-DD");
-      const endOfHoliday = endOfMonth.clone().format("YYYY-MM-DD");
-      //console.log("startOfHoliday   endOfHoliday",startOfMonth,endOfMonth)
-      const res = await fetch(
-        `/api/events?startOfHoliday=${startOfHoliday}&endOfHoliday=${endOfHoliday}`
-      );
-      const holidaysData = await res.json();
-      console.log("holidaysData", holidaysData);
-      const newHoliday = []
-      holidaysData?.holidayData.map((holiday)=>{
-        const newdate= moment.tz(holiday.event_date, 'UTC').format('YYYY-MM-DD')
-        newHoliday.push({date:newdate, name: holiday.nameEn})
-        console.log("holiday", newdate)
-      
-      })
-      console.log("newHoliday", newHoliday)
-
-      setholidaysOfMonth(() => newHoliday);
-    };
-    
-    startOfMonth && getMonHolidays();
-  }, [today]);
-  //console.log("holidaysOfMonth",holidaysOfMonth)
 
   useEffect(() => {
     const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
@@ -131,7 +82,7 @@ const MonthlyCalendar = (props) => {
   return (
     <div>
       <div>
-        <Container sx = {{alignContent:"center"}}>
+        <Container sx={{ alignContent: "center" }}>
           <br />
           <MonthlyCalendarHeader
             weekdayHeaders={weekdayHeaders}
@@ -150,48 +101,27 @@ const MonthlyCalendar = (props) => {
                   <div className="Empty-div"></div>
 
                   <div className="text">{day.value}</div>
-                  <div className="monthlyShedule">
-                    {myMonSched?.map((schedule, index) => {
-                      const starttime = moment(schedule.starttime).format(
-                        "h:mm a"
-                      );
-                      const endtime = moment(schedule.endtime).format("h:mm a");
-                      const scheduleDate = moment(schedule.starttime).date();
 
-                      //console.log("starttime", starttime)
-                      //console.log("scheduleDate,schedule.starttime  ", scheduleDate)
-                      if (scheduleDate === day.value) {
-                        return (
-                          <div key={index}>
-                            {starttime} - {endtime}
-                          </div>
-                        );
-                      }
-                    })}
-                  </div>
-                  <div>
-                    {holidaysOfMonth?.map((holiday,index) =>{
-                      const data = moment(holiday.date,("YYYY-MM-DD")).date()
-                      //const data = holiday[0]
-                      console.log("data",moment(holiday.date,("YYYY-MM-DD")))
-                      if(data === day.value){
-                        return (
-                         <div className="holidays">
-                          {holiday.name}
-                         </div>
-                        )
-                      }
-                      //console.log("data", data)
-                    })}
-                      
-                    
-                  </div>
+                  <MonthlySchedBar
+                    startOfMonth={startOfMonth}
+                    storeId={storeId}
+                    userId={userId}
+                    setMonSched={setMonSched}
+                    myMonSched={myMonSched}
+                    today={today}
+                    day={day}
+                  />
+                  <DisplayHolidays
+                    today={today}
+                    holidaysOfMonth={holidaysOfMonth}
+                    setholidaysOfMonth={setholidaysOfMonth}
+                    startOfMonth={startOfMonth}
+                    day={day}
+                  />
                 </div>
               );
             })}
           </div>
-
-          <div></div>
         </Container>
       </div>
     </div>
