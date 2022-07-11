e26801015c24300243bd8013b1f243ea33df0cc5
Siri <sirilakku01@gmail.com>
2022-07-10 22:00:27 -0600

    Added code to fetch data from backend

--- src/components/calendar/monthlyCalendar/AddEvent.js
+++ src/components/calendar/monthlyCalendar/AddEvent.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./monthlyCalendar.css";
 
-const getEventsAll = async function () {
-  
-};
-
 const AddEvent = (props) => {
   const { addEvent, setAddEvent } = props;
+  const [eventDate,setEventDate] = useState()
+  const [eventName,setEventName] = useState()
+
+  async function addEveToCalendar(newEvent){
+    
+
+  }
 
   useEffect(() => {
-    // setAddEvent()
-  }, [addEvent]);
+    const addEventData = async () => {
+      //const res = await fetch(`/api/events/createEvents?eventDate=${eventDate}&eventName=${eventName}`)
+      //console.log(" res in add event", res)
+      //setAddEvent()
+    }
+    addEventData()
+  }, []);
+
 
   return (
     <div>
-      <button className="addEventButton"> +</button>
+      <button className="addEventButton" > +</button>
+      {/* onClick={addEveToCalendar()} */}
     </div>
   );
 };
