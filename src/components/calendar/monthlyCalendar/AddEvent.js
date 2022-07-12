ac6241283b64ea004f46b1968b07a56f3705fbdc
Siri <sirilakku01@gmail.com>
2022-07-11 19:09:37 -0600

    Added onclick to button

--- src/components/calendar/monthlyCalendar/AddEvent.js
+++ src/components/calendar/monthlyCalendar/AddEvent.js
@@ -1,19 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./monthlyCalendar.css";
 
+
 const AddEvent = (props) => {
-  const { addEvent, setAddEvent } = props;
+  const { today,addEvent, setAddEvent } = props;
+  
   const [eventDate,setEventDate] = useState()
   const [eventName,setEventName] = useState()
-
+  
   async function addEveToCalendar(newEvent){
-    
+    const newEventDate = today.clone().format("YYYY-MM-DD")  
+   //setAddEvent()
+    console.log("addEvent today", newEventDate)
 
   }
 
   useEffect(() => {
     const addEventData = async () => {
-      //const res = await fetch(`/api/events/createEvents?eventDate=${eventDate}&eventName=${eventName}`)
+      //const res = await fetch(`/api/events/createEvents?eventDate=${eventDate}&eventName=${eventName}`){
+      //   method:"POST",
+      //   headers:{ "Content-Type": "application/json", }
+      // },body:res,
+
       //console.log(" res in add event", res)
       //setAddEvent()
     }
@@ -23,8 +31,8 @@ const AddEvent = (props) => {
 
   return (
     <div>
-      <button className="addEventButton" > +</button>
-      {/* onClick={addEveToCalendar()} */}
+      <button className="addEventButton" onClick={addEveToCalendar}> +</button>
+      
     </div>
   );
 };
