85bf24f2e212353c107585844d2202a4ccc920f6
Siri <sirilakku01@gmail.com>
2022-07-14 21:25:03 -0600

    Added code to form

--- src/components/calendar/monthlyCalendar/AddEvent.js
+++ src/components/calendar/monthlyCalendar/AddEvent.js
@@ -1,54 +1,78 @@
+import { FormControl, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import "./monthlyCalendar.css";
 
-
 const AddEvent = (props) => {
-  const { today,addEvent, setAddEvent,holidaysOfMonth } = props;
-  //console.log("holidaysOfMonth",holidaysOfMonth)
-  const [eventDate,setEventDate] = useState()
-  const [eventName,setEventName] = useState()
+  const { today,day, addEvent, setAddEvent, holidaysOfMonth } = props;
+  //console.log("day",day)
+  const [eventDate, setEventDate] = useState();
+  const [eventName, setEventName] = useState();
   const [open, setOpen] = useState(false);
-  
-  async function addEveToCalendar(newEvent){
-    const newEventDate = today.clone().format("YYYY-MM-DD")  
-   //setAddEvent()
-    console.log("addEvent today", newEventDate)
-    setOpen(true)
+
+  async function addEveToCalendar(newEvent) {
+    const newEventDate = today.clone().format("YYYY-MM-DD")
+    setAddEvent()
+    //console.log("addEvent today", newEventDate);
+    setOpen(true);
   }
-  
-    const addEventData = async () => {
-      const userData= {
-        //sender: employee_calendar_events.user_id,
-        eventDate:eventDate,
-        eventName:eventName
-      }
-      const data = JSON.stringify(userData)
-      const res = await fetch(`/api/events/createEvents?eventDate=${eventDate}&eventName=${eventName}`,
-      {
-        method:"POST",
-        headers:{ "Content-Type": "application/json", }
-      ,body:data
-      })
-      console.log(" res in add event", res)
-      setAddEvent()
-    }
+
+  const addEventData = async (e) => {
+e.preventDefault();
+    const userData = {
+      //sender: employee_calendar_events.user_id,
+      eventDate: day.date,
+      eventName: eventName,
+    };
+    
+     const data = JSON.stringify(userData);
+     console.log("Event data")
+    const res = await fetch(`/api/createEvents`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    });
+    const response = await res.json()
+    //console.log(" res in add event", response);
+    setAddEvent();
+  };
+  // const changeDate =(e)=>{
+  //   const value = e.target.value;
+  //   //console.log("value", value)
+  //   setEventDate(value)
+  // }
+
+  const changeName =(e) => {
+    const value = e.target.value
+    //console.log("name value", value)
+    setEventName(value)
+  }
 
   return (
-
     <div>
-      <button className="addEventButton" onClick={addEveToCalendar}> +</button>
-      {open&&(<div className="addEventModal">
-        <form action="form" method="post"  onSubmit={addEventData}>
-
-        <label for="eventDate">Event Name:</label>
-        <input type="text" id="date" name="eventDate"/><br/><br/>
-        <label for="eventName">Event Name:</label>
-        <input type="text" id="lname" name="eventName"/><br/><br/>
-        <input type="submit" value="Submit"/>
-
-        </form>
-
-      </div>)}
+      <button className="addEventButton" onClick={addEveToCalendar}>
+        {" "}
+        +
+      </button>
+      {open && (
+        <div className="addEventModal">
+          <button className="eventclose" onClick={() => setOpen(false)}>
+            x
+          </button>
+          <FormControl  onClick={(e)=>addEventData(e)}>
+            <label for="eventDate">Event Date:</label>
+            {/*<input type="text" id="date" name="eventDate" onChange={changeDate} value={eventDate}/>*/}
+            <div>{day.date}</div>
+            <br />
+            <br />
+            {/* <label for="eventName">Event Name:</label>
+            <input type="text" id="name" name="eventName"  onChange={changeName} value={eventName}/> */}
+            <TextField label={'Event Name'} id="event-name" margin="dense" />
+            <br />
+            <br />
+            <input type="submit" value="Submit" />
+          </FormControl>
+        </div>
+      )}
     </div>
   );
 };
