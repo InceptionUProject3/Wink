8a4b187ae82e55c2ee28b7f65c157ab399b51b1d
lmifflen <lmifflen@gmail.com>
2022-07-22 14:51:59 -0600

    fixed send message bug

--- src/components/messaging/Message.jsx
+++ src/components/messaging/Message.jsx
@@ -12,22 +12,37 @@ import { MessageContext } from "./MessageContext";
 const Message = () => {
   const authContext = useContext(LoginContext);
   const storeContext = useContext(StoreContext);
+  
   const user = storeContext.store;
 
   const [message, setMessage] = useState("");
+  const [receiver, setReceiver] = useState("");
   const location = useLocation();
 
   const socket = useContext(MessageContext).socketRef;
 
+  useEffect(() => {
+    location.state.profile ? (
+      setReceiver(location.state.profile.User_idUser)
+    ) : (
+      setReceiver(location.state.message.sender)
+    );
+  }
+  , [location.state]);
+
+
   const sendMessage = async (message) => {
+   
     try {
+ 
       const chat = {
         sender: user.User_idUser,
-        receiver: location.state.profile.User_idUser,
+        receiver: receiver,
         chat: message,
         store: user.Store_idStore,
       };
-      console.log("location", location.state.profile.User_idUser);
+
+      console.log("location", receiver);
       console.log("sending message", chat);
       const data = JSON.stringify(chat);
       const response = await fetch("/api/createconversation", {
@@ -39,7 +54,7 @@ const Message = () => {
       });
       if (response.status === 200) {
         let notify = {
-          user: location.state.profile.User_idUser,
+          // user: location.state.profile.User_idUser,
           store: user.Store_idStore,
         }
         console.log("sending notification", notify);
@@ -52,8 +67,14 @@ const Message = () => {
         console.log("failed to send message");
       }
     } catch (err) {
+      console.log("failed to send to user", user);
+      console.log("failed to send store", location.state.profile);
+      console.log("failed to send message", location.state.message);
+      console.log("error sending message", receiver);
       console.log("error sending message", err);
     }
+  
+
   };
 
   return (
@@ -84,3 +105,43 @@ const Message = () => {
 };
 
 export default Message;
+
+
+ // } } else { 
+    //   try {
+    //     const chat = {
+    //       sender: user.User_idUser,
+    //       receiver: location.state.message.receiver,
+    //       chat: message,
+    //       store: user.Store_idStore,
+    //     };
+    //     console.log("location", location.state.profile.User_idUser);
+    //     console.log("sending message", chat);
+    //     const data = JSON.stringify(chat);
+    //     const response = await fetch("/api/createconversation", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: data,
+    //     });
+    //     if (response.status === 200) {
+    //       let notify = {
+    //         // user: location.state.profile.User_idUser,
+    //         store: user.Store_idStore,
+    //       }
+    //       console.log("sending notification", notify);
+    //       socket.emit("notify", notify);
+    //       setMessage("");
+    //       console.log(response);
+    //       const theMessage = JSON.parse(await response.text());
+    //       console.log("we have the message", theMessage);
+    //     } else {
+    //       console.log("failed to send message");
+    //     }
+    //   } catch (err) {
+    //     console.log("failed to send to user", user);
+    //     console.log("failed to send store", location.state.profile);
+    //     console.log("failed to send message", location.state.message);
+    //     console.log("error sending message", err);
+    //   } }
