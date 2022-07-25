3c95041ffea7624a8a9c1ef487b258e61d1f4d7d
lmifflen <lmifflen@gmail.com>
2022-07-25 09:45:20 -0600

    removed unused components

--- src/App.js
+++ src/App.js
@@ -7,29 +7,25 @@ import LoginForm from "./components/authentication/LoginForm";
 import RequireAuth from "./components/authentication/RequireAuth";
 import LoginProvider from "./components/authentication/LoginProvider";
 
-import HomePage from "./pages/homepage/HomePage";
-import Messaging from "./components/messaging/Messaging";
-
 import MonthlyCalendar from "./components/calendar/monthlyCalendar/MonthlyCalendar";
 // import DisplayHolidays from "./components/calendar/monthlyCalendar/DisplayHolidays"
 import Calendar from "./pages/calendar/Calendar";
 import Logout from "./components/authentication/Logout";
 import ProfileSelection from "./components/authentication/LocationSelection";
 import { StoreProvider } from "./components/authentication/StoreProvider";
-import FindCoworkers from "./components/messaging/FindCoworkers";
-import Message from "./components/messaging/Message";
-import MessageWindow from "./components/messaging/MessageWindow";
+
 import Messenger from "./pages/messanger/Messenger";
 
 import AdminSchedule from "./pages/adminSchedule/AdminSchedule";
-import MessageProvider, { MessageContext } from "./components/messaging/MessageContext";
+import MessageProvider, {
+  MessageContext,
+} from "./components/messaging/MessageContext";
 import PrivateRoute from "./components/authentication/PrivateRoute";
 import Contacts from "./pages/messanger/Contacts";
 
 const SERVER = "http://localhost:4000";
 
 function App() {
-  
   return (
     <div className="App">
       <LoginProvider>
@@ -37,14 +33,12 @@ function App() {
           <MessageProvider>
             <BrowserRouter>
               <Navbar />
-              
 
-              
               <Routes>
                 {/* <Route path="/location" element={<Location />} /> */}
                 <Route path="/selection" element={<ProfileSelection />} />
 
-                <Route path="/coworkers" element={<Contacts/>} />
+                <Route path="/coworkers" element={<Contacts />} />
 
                 <Route path="/messenger" element={<Messenger />} />
 
@@ -58,14 +52,13 @@ function App() {
                 <Route path="/" element={<LoginForm />} />
                 <Route path="/logout" element={<Logout />} />
 
-              {/* admin route */}
-              {/* <Route path="/admin/schedule" mustBeAdmin element={<PrivateRoute element={<AdminSchedule />}/>} /> */}
-              <Route path="/admin/schedule" element={<AdminSchedule />} />
-              
-              {/* <Route path="/events" element={<DisplayHolidays />} /> */}
-            </Routes>
-            
-          </BrowserRouter>
+                {/* admin route */}
+                {/* <Route path="/admin/schedule" mustBeAdmin element={<PrivateRoute element={<AdminSchedule />}/>} /> */}
+                <Route path="/admin/schedule" element={<AdminSchedule />} />
+
+                {/* <Route path="/events" element={<DisplayHolidays />} /> */}
+              </Routes>
+            </BrowserRouter>
           </MessageProvider>
         </StoreProvider>
       </LoginProvider>
