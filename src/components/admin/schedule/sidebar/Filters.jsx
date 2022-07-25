736265eefda741ec7b87b7b40074d863b92f24d3
Inyoung Park <rkduddl1rl@gmail.com>
2022-07-25 01:29:03 -0600

    scheduling - all button in filters

--- src/components/admin/schedule/sidebar/Filters.jsx
+++ src/components/admin/schedule/sidebar/Filters.jsx
@@ -1,9 +1,14 @@
+import { positions } from "@mui/system";
 import React from "react";
+import { useState } from "react";
 import { TbRefresh } from "react-icons/tb";
 
 const Filters = (props) => {
-  const { filters, setFilters, setResetFilter} = props;
-  
+  const { filters, setFilters, setResetFilter } = props;
+  const [filterChecked, setFilterChecked] = useState({
+    availability: true,
+    positions: true,
+  });
   const onClickHrs = (e) => {
     const { name } = e.target;
     const newFilterArray = [];
@@ -53,16 +58,58 @@ const Filters = (props) => {
       return { ...pre, positions: newFilterArray };
     });
   };
-  
+//Allbuttons
+  const onNOffAvailFilters = (e) => {
+    const newFilterArray = [];
+    filters?.hours.map((hr) => {
+      return newFilterArray.push({
+        type: hr.type,
+        value: !filterChecked.availability,
+        min: hr.min,
+        max: hr.max,
+      });
+    });
+    setFilters((pre) => {
+      return { ...pre, hours: newFilterArray };
+    });
+    setFilterChecked((pre) => {
+      return { ...pre, availability: !filterChecked.availability };
+    });
+  };
+
+  const onNOffPosFilters = (e) => {
+    const newFilterArray = [];
+    filters?.positions.map((pos) => {
+      return newFilterArray.push({
+        id: pos.id,
+        type: pos.type,
+        value: !filterChecked.positions,
+        color: pos.color,
+      });
+    });
+    setFilters((pre) => {
+      return { ...pre, positions: newFilterArray };
+    });
+    setFilterChecked((pre) => {
+      return { ...pre, positions: !filterChecked.positions };
+    });
+  };
+
   return (
     <div className="filters">
       <div className="filters-refreshNtitle">
         <TbRefresh onClick={() => setResetFilter((pre) => !pre)} />
       </div>
-     <div className={`filters-container`}>
+      <div className={`filters-container`}>
         <div className="hours-container">
           <label>Availabilty</label>
           <div className="hours">
+            <button
+              onClick={onNOffAvailFilters}
+              className={`all_button ${filterChecked.availability}`}
+            >
+              All
+            </button>
             {filters?.hours?.map((hr, i) => {
               const checked = hr.value;
               return (
@@ -82,6 +129,12 @@ const Filters = (props) => {
         <div className="positions-container">
           <label>Positions</label>
           <div className="positions">
+            <button
+              onClick={onNOffPosFilters}
+              className={`all_button ${filterChecked.positions}`}
+            >
+              All
+            </button>
             {filters?.positions?.map((p, i) => {
               const checked = p.value;
 
